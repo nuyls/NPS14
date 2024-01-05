4f87bd38d6116946034666d21a053ad784c88d04
NpS <preeyanan.nps@gmail.com>
2024-01-05 15:14:28 +0700

    14.1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for(int i = 1; i < N; i++){
+        T current = d[i];
+        int j = i-1;
+        while(j >= 0 && d[j] < current){
+            d[j + 1] = d[j];
+            j--;
+        }
+        d[j+1] = current;
+        cout << "Pass " << i << ":";
+        for(int k = 0; k < N; k++) cout << d[k] << " ";
+        cout << "\n";
+    }
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +29,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
